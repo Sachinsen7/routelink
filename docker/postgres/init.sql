abef2729095d1abce70907c608792ba5dbd0fb77
Sachinsen7 <sachinsen1920@gmail.com>
2026-04-03 21:34:14 +0530

    Add PostgreSQL initialization SQL

---
+++ docker/postgres/init.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE routelink_auth;
+CREATE DATABASE routelink_user;
+CREATE DATABASE routelink_route;
+CREATE DATABASE routelink_chat;
+CREATE DATABASE routelink_payment;
+CREATE DATABASE routelink_notification;
